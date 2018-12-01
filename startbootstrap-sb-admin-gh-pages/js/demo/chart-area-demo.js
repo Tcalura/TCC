676961034cde5ab7013646d37f2f0569753d0d4d
Thiago Calura <thiagocalura@outlook.com>
2018-12-01 19:41:43 -0200

    alterado valores e redimencionamento do grafico de vendas para o mes

--- startbootstrap-sb-admin-gh-pages/js/demo/chart-area-demo.js
+++ startbootstrap-sb-admin-gh-pages/js/demo/chart-area-demo.js
@@ -9,7 +9,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
     datasets: [{
-      label: "Sessions",
+      label: "Produtos",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [100, 300, 627, 839, 828, 682, 741, 900, 849, 970, 1000, 684, 895],
     }],
   },
   options: {
@@ -39,8 +39,8 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
-          maxTicksLimit: 5
+          max: 2000,
+          maxTicksLimit: 6
         },
         gridLines: {
           color: "rgba(0, 0, 0, .125)",
